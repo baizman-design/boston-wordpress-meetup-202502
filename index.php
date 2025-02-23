88fc09aa6b2e47d8c46f7dcaf03e7d8ec6eff49d
Saul Baizman <saul@baizmandesign.com>
2025-02-23 14:37:35 -0500

    1
    Add important variables.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// new version number. uses semantic versioning (https://semver.org).
+$new_version = '1.0.1';
+
+// filename of ZIP archive with 1.0.1 version.
+$zip_archive = 'my-private-plugin.zip';
+
+// filename to store http requests.
+$logfile = 'http-requests.log';
+
+// for error_log(). sends output to a file.
+$logfile_type = 3;
