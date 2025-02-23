2572edf8bc244c53e027ac46af5eb4976110015c
Saul Baizman <saul@baizmandesign.com>
2025-02-23 16:30:29 -0500

    3
    Load plugin data as an array and check for the presence of “Update URI.”

--- my-private-plugin/my-private-plugin.php
+++ my-private-plugin/my-private-plugin.php
@@ -7,3 +7,12 @@
  * Author URI:          https://baizmandesign.com
  * Update URI:          https://private-repository.baizmandesign.com
  */
+
+defined( 'ABSPATH' ) || die ( 'This file cannot be run outside of WordPress.' );
+
+// get plugin header info as an array.
+$plugin_metadata = get_file_data(__FILE__, [ 'Version' => 'Version', 'Update URI' => 'Update URI', 'Description' => 'Description', 'Plugin Name' => 'Plugin Name', ]);
+if (! $plugin_metadata['Update URI'] ) {
+	// do nothing.
+	return;
+}
