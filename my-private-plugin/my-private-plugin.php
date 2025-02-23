22086d64aa6b49dcc59c8758e89b728d7d66c507
Saul Baizman <saul@baizmandesign.com>
2025-02-23 15:22:42 -0500

    2
    Add plugin header.
    
    TODO: activate the plugin.

--- my-private-plugin/my-private-plugin.php
+++ my-private-plugin/my-private-plugin.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ * Plugin Name:         My Private Plugin
+ * Description:         A private plugin for a client website.
+ * Version:             1.0.0
+ * Author:              Saul Baizman
+ * Author URI:          https://baizmandesign.com
+ * Update URI:          https://private-repository.baizmandesign.com
+ */
